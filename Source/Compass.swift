4ae986e25705966effd5d5917e1899e3ba2ff7e8
Christoffer Winterkvist <christoffer@hyper.no>
2015-12-14 15:21:45 +0100

    Add parseAsURL method

--- Source/Compass.swift
+++ Source/Compass.swift
@@ -17,6 +17,8 @@ public struct Compass {
     var result = false
     let query = url.absoluteString.substringFromIndex(scheme.endIndex)
 
+    guard !query.containsString("/") else { return parseAsURL(url, completion: completion) }
+
     for route in routes.sort({ $0 < $1 }) {
       guard let prefix = (route.characters
         .split { $0 == "{" }
@@ -25,8 +27,8 @@ public struct Compass {
 
       if query.hasPrefix(prefix) || prefix.hasPrefix(query) {
         let queryString = query.stringByReplacingOccurrencesOfString(prefix, withString: "")
-        let queryArguments = paths(queryString)
-        let routeArguments = paths(route).filter { $0.containsString("{") }
+        let queryArguments = splitString(queryString, delimiter: ":")
+        let routeArguments = splitString(route, delimiter: ":").filter { $0.containsString("{") }
 
         var arguments = [String : String]()
 
@@ -46,6 +48,23 @@ public struct Compass {
     return result
   }
 
+  static func parseAsURL(url: NSURL, completion: ParseCompletion) -> Bool {
+    guard let route = url.host else { return false }
+
+    var arguments = [String : String]()
+
+    [url.fragment, url.query].forEach {
+      splitString($0, delimiter: "&").forEach {
+        let pair = splitString($0, delimiter: "=")
+        arguments[pair[0]] = pair[1]
+      }
+    }
+
+    completion(route: route, arguments: arguments)
+
+    return true
+  }
+
   public static func navigate(urn: String, scheme: String = Compass.scheme) {
     let stringURL = "\(scheme)\(urn)"
     guard let url = NSURL(string: stringURL) else { return }
@@ -55,9 +74,11 @@ public struct Compass {
 
   // MARK: - Private Helpers
 
-  private static func paths(urn: String) -> [String] {
-    return urn.characters
-      .split { $0 == ":" }
+  private static func splitString(string: String?, delimiter: Character) -> [String] {
+    guard let string = string else { return [] }
+
+    return string.characters
+      .split { $0 == delimiter }
       .map(String.init)
   }
 }
