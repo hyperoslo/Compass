301bb7265f56626cd6fb43afb4f18ae3f1514486
Vadym Markov <markov.vadym@gmail.com>
2015-12-22 15:28:41 +0100

    Add test for URL with no slash

--- Tests/TestCompass.swift
+++ Tests/TestCompass.swift
@@ -94,9 +94,26 @@ class TestCompass: XCTestCase {
     self.waitForExpectationsWithTimeout(4.0, handler:nil)
   }
 
+  func testParseRegularURLWithSlashQuery() {
+    let expectation = self.expectationWithDescription("Parse URL with slash query")
+    let url = NSURL(string: "compassTests://callback/?access_token=Yo0OMrVZbRWNmgA6BT99hyuTUTNRGvqEEAQyeN1eslclzhFD0M8AidB4Z7Vs2NU8WoSNW0vYb961O38l&token_type=Bearer&expires_in=3600")!
+
+    Compass.parse(url) { route, arguments in
+      XCTAssertEqual(route, "callback")
+      XCTAssertEqual(arguments.count, 3)
+      XCTAssertEqual(arguments["access_token"], "Yo0OMrVZbRWNmgA6BT99hyuTUTNRGvqEEAQyeN1eslclzhFD0M8AidB4Z7Vs2NU8WoSNW0vYb961O38l")
+      XCTAssertEqual(arguments["expires_in"], "3600")
+      XCTAssertEqual(arguments["token_type"], "Bearer")
+
+      expectation.fulfill()
+    }
+
+    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+  }
+
   func testParseRegularURLWithQuery() {
     let expectation = self.expectationWithDescription("Parse URL with query")
-    let url = NSURL(string: "compassTests://callback/?access_token=Yo0OMrVZbRWNmgA6BT99hyuTUTNRGvqEEAQyeN1eslclzhFD0M8AidB4Z7Vs2NU8WoSNW0vYb961O38l&token_type=Bearer&expires_in=3600")!
+    let url = NSURL(string: "compassTests://callback?access_token=Yo0OMrVZbRWNmgA6BT99hyuTUTNRGvqEEAQyeN1eslclzhFD0M8AidB4Z7Vs2NU8WoSNW0vYb961O38l&token_type=Bearer&expires_in=3600")!
 
     Compass.parse(url) { route, arguments in
       XCTAssertEqual(route, "callback")
