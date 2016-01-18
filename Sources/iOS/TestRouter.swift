0f012797f837cd66891ae254a72ef743bc418792
Vadym Markov <markov.vadym@gmail.com>
2016-01-18 11:34:39 +0100

    Router tests

--- Sources/iOS/TestRouter.swift
+++ Sources/iOS/TestRouter.swift
@@ -1,9 +1,37 @@
-//
-//  TestRouter.swift
-//  Compass
-//
-//  Created by Vadym Markov on 18/01/16.
-//  Copyright © 2016 Hyper Interaktiv AS. All rights reserved.
-//
-
 import Foundation
+import XCTest
+import Compass
+
+class TestRoute: Routable {
+
+  var resolved = false
+
+  func resolve(arguments: [String: String], navigationController: UINavigationController) {
+    resolved = true
+  }
+}
+
+class TestRouter: XCTestCase {
+
+  var router: Router!
+  var navigationController = UINavigationController()
+  var route: TestRoute!
+
+  override func setUp() {
+    router = Router()
+    route = TestRoute()
+  }
+
+  func testNavigateIfRouteRegistered() {
+    router.routes["test"] = route
+    router.navigate("test", arguments: [:], navigationController: navigationController)
+
+    XCTAssertTrue(route.resolved)
+  }
+
+  func testNavigateIfRouteNotRegistered() {
+    router.navigate("test", arguments: [:], navigationController: navigationController)
+
+    XCTAssertFalse(route.resolved)
+  }
+}
