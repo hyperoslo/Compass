35d8e2bd9f8d28ec422a1c6bf9573f33ffec2223
Christoffer Winterkvist <christoffer@hyper.no>
2016-06-02 14:12:33 +0200

    Add query to routable

--- Sources/iOS/Routable.swift
+++ Sources/iOS/Routable.swift
@@ -2,5 +2,5 @@ import UIKit
 
 public protocol Routable {
 
-  func resolve(arguments: [String: String], currentController: UIViewController)
+  func resolve(arguments: [String: String], query: [String : AnyObject], currentController: UIViewController)
 }
