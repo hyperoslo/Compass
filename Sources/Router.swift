ef6c25dfff2dfe4fd6b2ea1252da060905798d53
Vadym Markov <markov.vadym@gmail.com>
2016-08-24 01:15:24 +0200

    Handle errors

--- Sources/Router.swift
+++ Sources/Router.swift
@@ -1,16 +1,34 @@
 public protocol Routable {
 
-  func navigate(to location: Location, from currentController: Controller)
+  func navigate(to location: Location, from currentController: Controller) throws
+}
+
+public protocol ErrorRoutable {
+
+  func handle(routeError: ErrorType, from currentController: Controller)
+}
+
+public enum RouteError: ErrorType {
+  case NotFound
 }
 
 public struct Router: Routable {
 
   public var routes = [String: Routable]()
+  public var errorRoute: ErrorRoutable?
 
   public init() {}
 
   public func navigate(to location: Location, from currentController: Controller) {
-    guard let route = routes[location.path] else { return }
-    route.navigate(to: location, from: currentController)
+    guard let route = routes[location.path] else {
+      errorRoute?.handle(RouteError.NotFound, from: currentController)
+      return
+    }
+
+    do {
+      try route.navigate(to: location, from: currentController)
+    } catch {
+      errorRoute?.handle(error, from: currentController)
+    }
   }
 }
