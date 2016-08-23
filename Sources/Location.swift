e77f61309ee38940c9845802754b4cf945eb9a59
Vadym Markov <markov.vadym@gmail.com>
2016-08-24 00:50:15 +0200

    Add payload to location

--- Sources/Location.swift
+++ Sources/Location.swift
@@ -3,14 +3,19 @@ public struct Location {
   public let path: String
   public let arguments: [String: String]
   public let fragments: [String: AnyObject]
+  public let payload: Any?
 
   public var scheme: String {
     return Compass.scheme
   }
 
-  public init(path: String, arguments: [String: String] = [:], fragments: [String: AnyObject] = [:]) {
+  public init(path: String,
+              arguments: [String: String] = [:],
+              fragments: [String: AnyObject] = [:],
+              payload: Any? = nil) {
     self.path = path
     self.arguments = arguments
     self.fragments = fragments
+    self.payload = payload
   }
 }
