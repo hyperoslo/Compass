134bd5fcb22a1523b7000a9e1118c5e22149cf08
Vadym Markov <markov.vadym@gmail.com>
2016-08-23 13:30:25 +0200

    Update tests

--- CompassTests/Shared/TestCompass.swift
+++ CompassTests/Shared/TestCompass.swift
@@ -27,271 +27,237 @@ class TestCompass: XCTestCase {
   }
 
   func testParseArguments() {
-    let expectation = self.expectationWithDescription("Parse arguments")
     let url = NSURL(string: "compassTests://profile:testUser")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual("profile:{user}", route)
-      XCTAssertEqual(arguments["user"], "testUser")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual("profile:{user}", route.route)
+    XCTAssertEqual(route.arguments["user"], "testUser")
   }
 
   func testParseFragments() {
-    let expectation = self.expectationWithDescription("Parse arguments")
     let url = NSURL(string: "compassTests://profile:testUser")!
 
-    Compass.parse(url, fragments: ["meta" : "foo"]) { route, arguments, fragments in
-      XCTAssertEqual("profile:{user}", route)
-      XCTAssertEqual(arguments["user"], "testUser")
-      XCTAssertEqual("foo" , fragments["meta"] as? String)
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url, fragments: ["meta" : "foo"]) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual("profile:{user}", route.route)
+    XCTAssertEqual(route.arguments["user"], "testUser")
+    XCTAssertEqual("foo" , route.fragments["meta"] as? String)
   }
 
   func testParseRouteConcreateMatchCount() {
-    let expectation = self.expectationWithDescription("Parse route having concrete match count")
     let url = NSURL(string: "compassTests://profile:admin")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual("profile:admin", route)
-      XCTAssert(arguments.isEmpty)
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual("profile:admin", route.route)
+    XCTAssert(route.arguments.isEmpty)
   }
 
   func testParseRouteWildcardMatchCount() {
-    let expectation = self.expectationWithDescription("Parse route having wildcard match count")
     let url = NSURL(string: "compassTests://profile:jack")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual("profile:{user}", route)
-      XCTAssertEqual(arguments["user"], "jack")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual("profile:{user}", route.route)
+    XCTAssertEqual(route.arguments["user"], "jack")
   }
 
   func testParseRouteSamePrefix() {
-    let expectation = self.expectationWithDescription("Parse route having same prefix")
     let url = NSURL(string: "compassTests://user:list")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual("user:list", route)
-      XCTAssert(arguments.isEmpty)
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual("user:list", route.route)
+    XCTAssert(route.arguments.isEmpty)
   }
 
   func testParseMultipleArguments() {
-    let expectation = self.expectationWithDescription("Parse multiple arguments")
     let url = NSURL(string: "compassTests://user:list:1:admin")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual("user:list:{userId}:{kind}", route)
-      XCTAssertEqual(arguments["userId"], "1")
-      XCTAssertEqual(arguments["kind"], "admin")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual("user:list:{userId}:{kind}", route.route)
+    XCTAssertEqual(route.arguments["userId"], "1")
+    XCTAssertEqual(route.arguments["kind"], "admin")
   }
 
   func testParseMultipleArgumentsWithFirstWildcard() {
-    let expectation = self.expectationWithDescription("Parse multiple arguments with first wild card")
     let url = NSURL(string: "compassTests://12:user:list:1:admin")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual("{appId}:user:list:{userId}:{kind}", route)
-      XCTAssertEqual(arguments["appId"], "12")
-      XCTAssertEqual(arguments["userId"], "1")
-      XCTAssertEqual(arguments["kind"], "admin")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual("{appId}:user:list:{userId}:{kind}", route.route)
+    XCTAssertEqual(route.arguments["appId"], "12")
+    XCTAssertEqual(route.arguments["userId"], "1")
+    XCTAssertEqual(route.arguments["kind"], "admin")
   }
 
   func testParseWithoutArguments() {
-    let expectation = self.expectationWithDescription("Parse without arguments")
     let url = NSURL(string: "compassTests://login")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual("login", route)
-      XCTAssert(arguments.isEmpty)
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual("login", route.route)
+    XCTAssert(route.arguments.isEmpty)
   }
 
   func testParseRegularURLWithFragments() {
-    let expectation = self.expectationWithDescription("Parse URL with fragments")
     let url = NSURL(string: "compassTests://callback/#access_token=IjvcgrkQk1p7TyJxKa26rzM1wBMFZW6XoHK4t5Gkt1xQLTN8l7ppR0H3EZXpoP0uLAN49oCDqTHsvnEV&token_type=Bearer&expires_in=3600")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual(route, "callback")
-      XCTAssertEqual(arguments.count, 3)
-      XCTAssertEqual(arguments["access_token"], "IjvcgrkQk1p7TyJxKa26rzM1wBMFZW6XoHK4t5Gkt1xQLTN8l7ppR0H3EZXpoP0uLAN49oCDqTHsvnEV")
-      XCTAssertEqual(arguments["expires_in"], "3600")
-      XCTAssertEqual(arguments["token_type"], "Bearer")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual(route.route, "callback")
+    XCTAssertEqual(route.arguments.count, 3)
+    XCTAssertEqual(route.arguments["access_token"], "IjvcgrkQk1p7TyJxKa26rzM1wBMFZW6XoHK4t5Gkt1xQLTN8l7ppR0H3EZXpoP0uLAN49oCDqTHsvnEV")
+    XCTAssertEqual(route.arguments["expires_in"], "3600")
+    XCTAssertEqual(route.arguments["token_type"], "Bearer")
   }
 
   func testParseRegularURLWithFragmentsAndGoogleOAuth2AccessToken() {
-    let expectation = self.expectationWithDescription("Parse URL with fragments and Google OAuth 2.0 access token format")
     let url = NSURL(string: "compassTests://callback/#access_token=ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ&token_type=Bearer&expires_in=3600")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual(route, "callback")
-      XCTAssertEqual(arguments.count, 3)
-      XCTAssertEqual(arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ")
-      XCTAssertEqual(arguments["expires_in"], "3600")
-      XCTAssertEqual(arguments["token_type"], "Bearer")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual(route.route, "callback")
+    XCTAssertEqual(route.arguments.count, 3)
+    XCTAssertEqual(route.arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ")
+    XCTAssertEqual(route.arguments["expires_in"], "3600")
+    XCTAssertEqual(route.arguments["token_type"], "Bearer")
   }
 
   func testParseRegularURLWithFragmentsAndAlternativeAccessToken() {
-    let expectation = self.expectationWithDescription("Parse URL with fragments and alternative access token format")
     let url = NSURL(string: "compassTests://callback/#access_token=ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ=&token_type=Bearer&expires_in=3600")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual(route, "callback")
-      XCTAssertEqual(arguments.count, 3)
-      XCTAssertEqual(arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ=")
-      XCTAssertEqual(arguments["expires_in"], "3600")
-      XCTAssertEqual(arguments["token_type"], "Bearer")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual(route.route, "callback")
+    XCTAssertEqual(route.arguments.count, 3)
+    XCTAssertEqual(route.arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ=")
+    XCTAssertEqual(route.arguments["expires_in"], "3600")
+    XCTAssertEqual(route.arguments["token_type"], "Bearer")
   }
 
   func testParseRegularURLWithSlashQuery() {
-    let expectation = self.expectationWithDescription("Parse URL with slash query")
     let url = NSURL(string: "compassTests://callback/?access_token=Yo0OMrVZbRWNmgA6BT99hyuTUTNRGvqEEAQyeN1eslclzhFD0M8AidB4Z7Vs2NU8WoSNW0vYb961O38l&token_type=Bearer&expires_in=3600")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual(route, "callback")
-      XCTAssertEqual(arguments.count, 3)
-      XCTAssertEqual(arguments["access_token"], "Yo0OMrVZbRWNmgA6BT99hyuTUTNRGvqEEAQyeN1eslclzhFD0M8AidB4Z7Vs2NU8WoSNW0vYb961O38l")
-      XCTAssertEqual(arguments["expires_in"], "3600")
-      XCTAssertEqual(arguments["token_type"], "Bearer")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual(route.route, "callback")
+    XCTAssertEqual(route.arguments.count, 3)
+    XCTAssertEqual(route.arguments["access_token"], "Yo0OMrVZbRWNmgA6BT99hyuTUTNRGvqEEAQyeN1eslclzhFD0M8AidB4Z7Vs2NU8WoSNW0vYb961O38l")
+    XCTAssertEqual(route.arguments["expires_in"], "3600")
+    XCTAssertEqual(route.arguments["token_type"], "Bearer")
   }
 
   func testParseRegularURLWithSlashQueryAndGoogleOAuth2AccessToken() {
-    let expectation = self.expectationWithDescription("Parse URL with slash query and Google OAuth 2.0 access token format")
     let url = NSURL(string: "compassTests://callback/?access_token=ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ&token_type=Bearer&expires_in=3600")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual(route, "callback")
-      XCTAssertEqual(arguments.count, 3)
-      XCTAssertEqual(arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ")
-      XCTAssertEqual(arguments["expires_in"], "3600")
-      XCTAssertEqual(arguments["token_type"], "Bearer")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual(route.route, "callback")
+    XCTAssertEqual(route.arguments.count, 3)
+    XCTAssertEqual(route.arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ")
+    XCTAssertEqual(route.arguments["expires_in"], "3600")
+    XCTAssertEqual(route.arguments["token_type"], "Bearer")
   }
 
   func testParseRegularURLWithSlashQueryAndAlternativeAccessToken() {
-    let expectation = self.expectationWithDescription("Parse URL with slash query and alternative access token format")
     let url = NSURL(string: "compassTests://callback/?access_token=ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ=&token_type=Bearer&expires_in=3600")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual(route, "callback")
-      XCTAssertEqual(arguments.count, 3)
-      XCTAssertEqual(arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ=")
-      XCTAssertEqual(arguments["expires_in"], "3600")
-      XCTAssertEqual(arguments["token_type"], "Bearer")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual(route.route, "callback")
+    XCTAssertEqual(route.arguments.count, 3)
+    XCTAssertEqual(route.arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ=")
+    XCTAssertEqual(route.arguments["expires_in"], "3600")
+    XCTAssertEqual(route.arguments["token_type"], "Bearer")
   }
 
   func testParseRegularURLWithQuery() {
-    let expectation = self.expectationWithDescription("Parse URL with query")
     let url = NSURL(string: "compassTests://callback?access_token=Yo0OMrVZbRWNmgA6BT99hyuTUTNRGvqEEAQyeN1eslclzhFD0M8AidB4Z7Vs2NU8WoSNW0vYb961O38l&token_type=Bearer&expires_in=3600")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual(route, "callback")
-      XCTAssertEqual(arguments.count, 3)
-      XCTAssertEqual(arguments["access_token"], "Yo0OMrVZbRWNmgA6BT99hyuTUTNRGvqEEAQyeN1eslclzhFD0M8AidB4Z7Vs2NU8WoSNW0vYb961O38l")
-      XCTAssertEqual(arguments["expires_in"], "3600")
-      XCTAssertEqual(arguments["token_type"], "Bearer")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual(route.route, "callback")
+    XCTAssertEqual(route.arguments.count, 3)
+    XCTAssertEqual(route.arguments["access_token"], "Yo0OMrVZbRWNmgA6BT99hyuTUTNRGvqEEAQyeN1eslclzhFD0M8AidB4Z7Vs2NU8WoSNW0vYb961O38l")
+    XCTAssertEqual(route.arguments["expires_in"], "3600")
+    XCTAssertEqual(route.arguments["token_type"], "Bearer")
   }
 
   func testParseRegularURLWithQueryAndGoogleOAuth2AccessToken() {
-    let expectation = self.expectationWithDescription("Parse URL with query and Google OAuth 2.0 access token format")
     let url = NSURL(string: "compassTests://callback?access_token=ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ&token_type=Bearer&expires_in=3600")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual(route, "callback")
-      XCTAssertEqual(arguments.count, 3)
-      XCTAssertEqual(arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ")
-      XCTAssertEqual(arguments["expires_in"], "3600")
-      XCTAssertEqual(arguments["token_type"], "Bearer")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual(route.route, "callback")
+    XCTAssertEqual(route.arguments.count, 3)
+    XCTAssertEqual(route.arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ")
+    XCTAssertEqual(route.arguments["expires_in"], "3600")
+    XCTAssertEqual(route.arguments["token_type"], "Bearer")
   }
 
   func testParseRegularURLWithQueryAndAlternativeAccessToken() {
-    let expectation = self.expectationWithDescription("Parse URL with query and alternative access token format")
     let url = NSURL(string: "compassTests://callback?access_token=ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ=&token_type=Bearer&expires_in=3600")!
 
-    Compass.parse(url) { route, arguments, _ in
-      XCTAssertEqual(route, "callback")
-      XCTAssertEqual(arguments.count, 3)
-      XCTAssertEqual(arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ=")
-      XCTAssertEqual(arguments["expires_in"], "3600")
-      XCTAssertEqual(arguments["token_type"], "Bearer")
-
-      expectation.fulfill()
+    guard let route = Compass.parse(url) else {
+      XCTFail("Compass parsing failed")
+      return
     }
 
-    self.waitForExpectationsWithTimeout(4.0, handler:nil)
+    XCTAssertEqual(route.route, "callback")
+    XCTAssertEqual(route.arguments.count, 3)
+    XCTAssertEqual(route.arguments["access_token"], "ya29.Ci8nA1pNVMFffHkS5-sXooNGvTB9q8QPtoM56sWpipRyjhwwEiKyZxvRQTR8saqWzQ=")
+    XCTAssertEqual(route.arguments["expires_in"], "3600")
+    XCTAssertEqual(route.arguments["token_type"], "Bearer")
   }
 }
