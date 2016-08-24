6dedbf40d82315fc22b1501f2945e3db43fae67a
Vadym Markov <markov.vadym@gmail.com>
2016-08-24 10:18:12 +0200

    Remove fragments

--- Sources/Location.swift
+++ Sources/Location.swift
@@ -2,20 +2,15 @@ public struct Location {
 
   public let path: String
   public let arguments: [String: String]
-  public let fragments: [String: AnyObject]
   public let payload: Any?
 
   public var scheme: String {
     return Compass.scheme
   }
 
-  public init(path: String,
-              arguments: [String: String] = [:],
-              fragments: [String: AnyObject] = [:],
-              payload: Any? = nil) {
+  public init(path: String, arguments: [String: String] = [:], payload: Any? = nil) {
     self.path = path
     self.arguments = arguments
-    self.fragments = fragments
     self.payload = payload
   }
 }
