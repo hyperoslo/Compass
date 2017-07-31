f29d64326499910b61044a962968af4e6c6174ea
Khoa Pham <onmyway133@gmail.com>
2017-07-31 12:58:58 +0200

    Remove unused property in Location

--- Sources/Location.swift
+++ Sources/Location.swift
@@ -11,11 +11,6 @@ public struct Location {
   /// An optional payload if you want to send in app objects
   public let payload: Any?
 
-  /// The application scheme
-  public var scheme: String {
-    return Navigator.scheme
-  }
-
   /// Construct a Location
   public init(path: String, arguments: [String: String] = [:], payload: Any? = nil) {
     self.path = path
