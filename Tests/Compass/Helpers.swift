a850d7d9c0e33ada04d4525f801e5ac3aa58ff1a
Khoa Pham <onmyway133@gmail.com>
2017-08-16 12:33:43 +0200

    Add testParseEncodeURL

--- Tests/Compass/Helpers.swift
+++ Tests/Compass/Helpers.swift
@@ -1,4 +1,5 @@
 import Foundation
+import XCTest
 @testable import Compass
 
 // MARK: - Routes
@@ -46,6 +47,20 @@ extension Array {
     }
 
     return list
+  }
+}
+
+extension XCTestCase {
+  func wait(for duration: TimeInterval) {
+    let waitExpectation = expectation(description: "Waiting")
+
+    let when = DispatchTime.now() + duration
+    DispatchQueue.main.asyncAfter(deadline: when) {
+      waitExpectation.fulfill()
+    }
+
+    // We use a buffer here to avoid flakiness with Timer on CI
+    waitForExpectations(timeout: duration + 0.5)
   }
 }
 
